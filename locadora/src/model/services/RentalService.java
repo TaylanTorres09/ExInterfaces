7c0255856af8bf2f869910fea00e227f47ccfcae
TaylanTorres09 <taylantorres@gmail.com>
2023-02-11 10:26:06 -0300

    Implements service to Invoices

---
+++ locadora/src/model/services/RentalService.java
@@ -0,0 +1,40 @@
+package model.services;
+
+import java.time.Duration;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerHour;
+	private Double pricePerDay;
+	
+	private TaxService taxService;
+
+	public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
+		this.pricePerHour = pricePerHour;
+		this.pricePerDay = pricePerDay;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		Double minutes = (double) Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		Double hours = minutes / 60.0;
+		
+		double basicPayment;
+		if (hours <= 12.0) {
+			basicPayment = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayment = pricePerDay * Math.ceil(hours / 24);
+		}
+
+		double tax = taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+		
+	}
+	
+}
