2a9ca70347bb1d3fb1c3c5d5860e10eec531c0b7
TaylanTorres09 <taylantorres@gmail.com>
2023-02-13 10:20:39 -0300

    Format String to class Installment

--- ExFixacao/src/model/entities/Installment.java
+++ ExFixacao/src/model/entities/Installment.java
@@ -1,8 +1,11 @@
 package model.entities;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Installment {
+	
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	private LocalDate dueDate;
 	private Double amount;
@@ -27,6 +30,11 @@ public class Installment {
 
 	public void setAmount(Double amount) {
 		this.amount = amount;
+	}
+
+	@Override
+	public String toString() {
+		return dueDate.format(fmt) + " - " + String.format("%.2f", amount);
 	}
 
 }
